dc5cb8d9e4b55a2206d11dfa5dc2b3682561b474
Bodyan322 <bogdan3206@gmail.com>
2019-02-01 17:01:56 +0200

    modify/JsFiles, add some changes to HttpRequest

--- src/HttpRequest.js
+++ src/HttpRequest.js
@@ -1,26 +1,20 @@
-function applyHeaders(XHR, headers) {
-  for (const key in headers) {
-    XHR.setRequestHeader(key, headers[key]);
-  }
-}
-
 class HttpRequest {
   constructor({ baseUrl, headers }) {
     this.baseUrl = baseUrl;
     this.headers = headers;
   }
 
-  static generateURL(urlString, baseURLString, params) {
+  static generateURL(urlString, baseURLString, parameters) {
     const url = new URL(urlString, baseURLString);
 
-    for (const key in params) {
-      url.searchParams.set(key, params[key]);
+    for (const key in parameters) {
+      url.searchParams.set(key, parameters[key]);
     }
     return url;
   }
 
   __request(url, method, config) {
-    const XHR = new HttpRequest();
+    const XHR = new XMLHttpRequest();
     const { headers, onDownloadProgress, params, responseType = 'json', data } = config;
     const finalURL = HttpRequest.generateURL(url, this.baseUrl, params);
     const headersObj = { ...headers, ...this.headers };
@@ -30,7 +24,7 @@ class HttpRequest {
       XHR.responseType = responseType;
 
       Object.entries(headersObj).forEach(([key, value]) => {
-        XHR.applyHeaders(key, value);
+        XHR.setRequestHeader(key, value);
       });
 
       XHR.onprogress = onDownloadProgress;
--- src/progress.js
+++ src/progress.js
@@ -17,35 +17,4 @@ function showImgOnPage(data) {
   const imgSrc = window.URL.createObjectURL(data, { type: `${data.type}` });
   document.getElementById('download-img').src = imgSrc;
 }
-function downloadFile(data) {
-  const downloadURL = URL.createObjectURL(data, { type: data.type });
-  const fileLink = document.createElement('a');
-
-  document.body.appendChild(fileLink);
-  fileLink.style.display = 'none';
-  fileLink.href = downloadURL;
-  fileLink.download = data.type;
-  fileLink.click();
-  document.body.removeChild(fileLink);
-}
-
-document.getElementById('downloadForm').onsubmit = function(e) {
-  e.preventDefault();
-  // eslint-disable-next-line no-undef
-  const xhr = new HttpRequest({ baseUrl: 'http://localhost:8000' });
-  xhr.get(`/files/${e.target[0].value}`, { onDownloadProgress, responseType: 'blob' })
-    .then(data => {
-      if (data.type === 'image/jpeg') {
-        showImgOnPage(data);
-      } else {
-        downloadFile(data);
-      }
-    })
-    .catch(error => {
-      throw new Error(error);
-    });
-  // eslint-disable-next-line no-undef
-  btnDownload.disabled = true;
-};
-
 
